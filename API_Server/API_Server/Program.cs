59e09c49317690578c48f08a33a0e655f19cd04c
Phi <0306211387@caothang.edu.vn>
2023-11-21 23:56:37 +0700

    init_test

--- API_Server/API_Server/Program.cs
+++ API_Server/API_Server/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddIdentity<User, IdentityRole>()
-				.AddEntityFrameworkStores<>()
+				.AddEntityFrameworkStores<API_ServerContext>()
 				.AddDefaultTokenProviders();
 
 builder.Services.AddAuthentication(options =>
