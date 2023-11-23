5bbe8346f0ffc95fd19c3ca0a30ab7650fa9fac7
Phi <0306211387@caothang.edu.vn>
2023-11-23 07:34:42 +0700

    init myapp
    footer

--- my-app/src/Layout.js
+++ my-app/src/Layout.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import SlideShow from './components/SlideShow';
+import Footer from './components/Footer';
 
 function Layout() {
   return (
     <>
         <Navbar/>
         <SlideShow/>
+        <Footer/>
     </>
   );
 }
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,48 +4,61 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-function NavScrollExample() {
+
+function OffcanvasExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+        <Navbar key="lg" expand="lg" className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">
+              <img style={{width:'100px'}} src="https://t3.ftcdn.net/jpg/05/36/02/06/360_F_536020641_rprZK7Kg8Cx6EtU7RJOjkLrdLNp6sB6a.jpg"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-lg`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-start flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Trang chủ</Nav.Link>
+                  <Nav.Link href="#action2">Sản phẩm</Nav.Link>
+                  <NavDropdown
+                    title="Thể loại"
+                    id={`offcanvasNavbarDropdown-expand-lg`}
+                  >
+                    <NavDropdown.Item href="#action3">Thể thao</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Y tá
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Khác
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Tìm kiếm"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success"><i class="fa-solid fa-magnifying-glass"></i></Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+    </>
   );
 }
 
-export default NavScrollExample;
+export default OffcanvasExample;
